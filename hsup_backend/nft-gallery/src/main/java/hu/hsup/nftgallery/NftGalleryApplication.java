3198b1e8d2b5c6507a3bc1412453fe599260158b
Enci <encsi2346@gmail.com>
2022-03-18 13:10:14 +0100

    First backend commit

---
+++ hsup_backend/nft-gallery/src/main/java/hu/hsup/nftgallery/NftGalleryApplication.java
@@ -0,0 +1,13 @@
+package hu.hsup.nftgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftGalleryApplication.class, args);
+	}
+
+}
---
+++ hsup_backend/nft-gallery/src/test/java/hu/hsup/nftgallery/NftGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hsup.nftgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
